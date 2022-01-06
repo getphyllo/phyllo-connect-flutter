8d834d81f18ca27395c108f77d564ef492c8e4ce
Pankaj Patel <patelpankaj.1010@gmail.com>
2022-01-06 19:27:23 +0530

    - removed client id

--- example/lib/constants/configs.dart
+++ example/lib/constants/configs.dart
@@ -3,9 +3,9 @@ import 'package:phyllo_connect/phyllo_connect.dart';
 class Configs {
   Configs._();
 
-  static const String clientId = '1ad05f8d-2fb1-44d0-896a-3ce509679b85';
+  static const String clientId = '';
 
-  static const String clientSecret = 'b9f94c18-efde-4ab2-bb8a-6d47299a5fa9';
+  static const String clientSecret = '';
 
   static const PhylloEnvironment environment = PhylloEnvironment.development;
 }
