f911f08129fc27dd3041fe1b337dbc3e96640e76
Pankaj Patel <patelpankaj.1010@gmail.com>
2022-01-17 15:41:29 +0530

    - config removed

--- example/lib/constants/configs.dart
+++ example/lib/constants/configs.dart
@@ -3,9 +3,9 @@ import 'package:phyllo_connect/phyllo_connect.dart';
 class Configs {
   Configs._();
 
-  static const String clientId = '2778c997-f4bc-401d-95c4-6f2178b85404';
+  static const String clientId = '';
 
-  static const String clientSecret = '535b916c-292b-4771-bd6a-41dc4a53db6f';
+  static const String clientSecret = '';
 
   static const PhylloEnvironment environment = PhylloEnvironment.development;
 }
