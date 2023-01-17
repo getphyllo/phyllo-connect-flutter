73cce2634370ad5171724c127f1348ee042c1529
Pankaj Patel <patelpankaj.1010@gmail.com>
2023-01-17 13:35:03 +0530

    PLATFORM-2493 Version update

--- lib/src/verison_constants.dart
+++ lib/src/verison_constants.dart
@@ -3,13 +3,13 @@ class VersionConstants {
 
   static const String flutterSdkVersion = '0.3.2';
 
-  static const String iosSdkVersion = '0.3.1';
+  static const String iosSdkVersion = '0.3.2';
 
   static const String minSupportedIosVersion = '12.0';
 
   static const String maxSupportedIosVersion = '16.0*';
 
-  static const String androidSdkVersion = '0.3.1';
+  static const String androidSdkVersion = '0.3.2';
 
   static const String minSupportedAndroidVersion = '21';
 
