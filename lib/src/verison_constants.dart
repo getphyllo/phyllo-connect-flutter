658469ab4eb14e9cd8cce31784c6b4e412a43894
Pankaj Patel <patelpankaj.1010@gmail.com>
2024-01-02 11:57:06 +0530

    Android SDK checking with Snapshot version
    - upgraded with android and constant version

--- lib/src/verison_constants.dart
+++ lib/src/verison_constants.dart
@@ -3,15 +3,15 @@ class VersionConstants {
 
   static const String flutterSdkVersion = '0.3.4';
 
-  static const String iosSdkVersion = '0.3.2';
+  static const String iosSdkVersion = '0.3.4';
 
   static const String minSupportedIosVersion = '12.0';
 
-  static const String maxSupportedIosVersion = '16.0*';
+  static const String maxSupportedIosVersion = '17.0*';
 
-  static const String androidSdkVersion = '0.3.3';
+  static const String androidSdkVersion = '0.3.4';
 
   static const String minSupportedAndroidVersion = '21';
 
-  static const String maxSupportedAndroidVersion = '31';
+  static const String maxSupportedAndroidVersion = '34';
 }
