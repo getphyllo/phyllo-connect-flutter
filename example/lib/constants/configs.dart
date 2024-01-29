a11942e58b2900b4012da5a58638235122500b88
Pankaj Patel <patelpankaj.1010@gmail.com>
2024-01-29 15:32:02 +0530

    Release 0.3.5
    - Upgraded compileSdkVersion and targetSdkVersion to API Version 34.
    - Ui style improvement (loader color change).

--- example/lib/constants/configs.dart
+++ example/lib/constants/configs.dart
@@ -3,9 +3,9 @@ import 'package:phyllo_connect/phyllo_connect.dart';
 class Configs {
   Configs._();
 
-  static const String clientId = '<client id here>';
+  static const String clientId = '0a62ced6-3b00-4112-9c6b-7e7e61db02d0';
   
-  static const String clientSecret = '<client secret here>';
+  static const String clientSecret = '6260ff9d-1026-4b75-8d81-b33e317b4581';
   
   static const PhylloEnvironment environment = PhylloEnvironment.sandbox;
 }
